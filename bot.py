218715d85964925c473b7832e1b58e0eff7db1f5
DimonSaur <81377704+DimonSaur@users.noreply.github.com>
2021-03-25 19:10:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,13 +8,13 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_hello(message: types.Message):
-      await bot.send_message(message.from_user.id, 'Привет! Связь со мной:\nTelegram: @dimonsaur\nVK: https://vk.com/dimonsaur',
+      await bot.send_message(message.from_user.id, 'Привет!',
                              reply_markup=kb.greet_kb)
 
 
 @dp.message_handler()
 async def process_reply(message: types.Message):
-      await message.reply('Мой сервер в Minecraft:\nhttps://hardserver.ml')
+      await message.reply('Кнопка')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
