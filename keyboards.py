e09471f5801b5a3eed95a8333e0d2e2c837fe74d
DimonSaur <81377704+DimonSaur@users.noreply.github.com>
2021-03-25 19:10:24 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -2,5 +2,5 @@ from aiogram.types import ReplyKeyboardMarkup, \
                         KeyboardButton
 
 # Одна кнопка
-btnContacts = KeyboardButton("Проект")
+btnContacts = KeyboardButton("Кнопка")
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(btnContacts)
